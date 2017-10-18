eb80172416165b904d9aabb5ce0f6208a0b87957
Brian pareja <brian.pareja@tecsup.edu.pe>
2017-10-18 14:48:10 -0500

    + auth x redes sociales al 50%

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : ' 832282320285641 ',
+        'clientSecret'  : ' f59ee167764aa4ef2ed14bde1f5ec3b9 ',
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+	'googleAuth' : {
+		'clientID' 		: '694374735549-telf0cjtdbqtf3gn3atb6utofv6mh251.apps.googleusercontent.com',
+		'clientSecret' 	: 'uNlTNdrSW2pO9Wfsj5s3Tvs9',
+		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+	}
+
+
+
+};
--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,20 @@
-
 var LocalStrategy    = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy;
+
 var User       = require('../app/models/user');
-var configAuth = require('./auth'); // use this one for testing
+
+var configAuth = require('./auth');
 
 module.exports = function(passport) {
-    passport.serializeUser(function (user, done) {
+
+    passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
 
-       passport.deserializeUser(function (id, done) {
-        User.findById(id, function (err, user) {
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
             done(err, user);
         });
     });
@@ -33,10 +38,10 @@ module.exports = function(passport) {
 
                     // if no user is found, return the message
                     if (!user)
-                        return done(null, false, req.flash('loginMessage', 'No user found.'));
+                        return done(null, false, req.flash('loginMessage', 'Wrong Information.'));
 
                     if (!user.validPassword(password))
-                        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+                        return done(null, false, req.flash('loginMessage', 'Wrong Information.'));
 
                     // all is well, return user
                     else
@@ -141,4 +146,202 @@ module.exports = function(passport) {
             });
 
         }));
-}
+
+
+    passport.use('local-pswchange', new LocalStrategy({
+            // by default, local strategy uses username and password, we will override with userID
+            usernameField: 'userID',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+        },
+        function (req, userID, password, done) {
+            if (userID)
+                userID = userID.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
+
+            // asynchronous
+            process.nextTick(function () {
+                User.findOne({'local.userID': userID}, function (err, user) {
+                    // if there are any errors, return the error
+                    if (err)
+                        return done(err);
+
+                    if (!user)
+                        return done(null, false, req.flash('loginMessage', 'Wrong Information.'));
+                    // all is well, return user
+                    else{
+                        user.local.password = user.generateHash(password);
+
+                        user.save(function (err) {
+                            if (err)
+                                return done(err);
+
+                            return done(null, user);
+                        });
+                    }
+
+
+                });
+            });
+
+        }));
+
+
+
+
+
+    console.log(configAuth.facebookAuth+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    var fbStrategy = configAuth.facebookAuth;
+    fbStrategy.passReqToCallback = true;  // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+    passport.use(new FacebookStrategy(fbStrategy,
+        function(req, token, refreshToken, profile, done) {
+
+            // asynchronous
+            process.nextTick(function() {
+
+                // check if the user is already logged in
+                if (!req.user) {
+
+                    User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                        if (err)
+                            return done(err);
+
+                        if (user) {
+
+                            // if there is a user id already but no token (user was linked at one point and then removed)
+                            if (!user.facebook.token) {
+                                user.facebook.token = token;
+                                user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                                user.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+                                user.save(function(err) {
+                                    if (err)
+                                        return done(err);
+
+                                    return done(null, user);
+                                });
+                            }
+
+                            return done(null, user); // user found, return that user
+                        } else {
+                            // if there is no user, create them
+                            var newUser            = new User();
+
+                            newUser.facebook.id    = profile.id;
+                            newUser.facebook.token = token;
+                            newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                            newUser.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+                            newUser.save(function(err) {
+                                if (err)
+                                    return done(err);
+
+                                return done(null, newUser);
+                            });
+                        }
+                    });
+
+                } else {
+                    // user already exists and is logged in, we have to link accounts
+                    var user            = req.user; // pull the user out of the session
+
+                    user.facebook.id    = profile.id;
+                    user.facebook.token = token;
+                    user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                    user.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+                    user.save(function(err) {
+                        if (err)
+                            return done(err);
+
+                        return done(null, user);
+                    });
+
+                }
+            });
+
+        }));
+
+    // =========================================================================
+    // TWITTER =================================================================
+    // =========================================================================
+    // =========================================================================
+    // GOOGLE ==================================================================
+    // =========================================================================
+    passport.use(new GoogleStrategy({
+
+            clientID        : configAuth.googleAuth.clientID,
+            clientSecret    : configAuth.googleAuth.clientSecret,
+            callbackURL     : configAuth.googleAuth.callbackURL,
+            passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+
+        },
+        function(req, token, refreshToken, profile, done) {
+
+            // asynchronous
+            process.nextTick(function() {
+
+                if (!req.user) {
+
+                    User.findOne({ 'google.id' : profile.id }, function(err, user) {
+                        if (err)
+                            return done(err);
+
+                        if (user) {
+
+                            if (!user.google.token) {
+                                user.google.token = token;
+                                user.google.name  = profile.displayName;
+                                user.google.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
+
+                                user.save(function(err) {
+                                    if (err)
+                                        return done(err);
+
+                                    return done(null, user);
+                                });
+                            }
+
+                            return done(null, user);
+                        } else {
+                            var newUser          = new User();
+
+                            newUser.google.id    = profile.id;
+                            newUser.google.token = token;
+                            newUser.google.name  = profile.displayName;
+                            newUser.google.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
+
+                            newUser.save(function(err) {
+                                if (err)
+                                    return done(err);
+
+                                return done(null, newUser);
+                            });
+                        }
+                    });
+
+                } else {
+                    // user already exists and is logged in, we have to link accounts
+                    var user               = req.user; // pull the user out of the session
+
+                    user.google.id    = profile.id;
+                    user.google.token = token;
+                    user.google.name  = profile.displayName;
+                    user.google.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
+
+                    user.save(function(err) {
+                        if (err)
+                            return done(err);
+
+                        return done(null, user);
+                    });
+
+                }
+
+            });
+
+        }));
+
+
+
+
+};

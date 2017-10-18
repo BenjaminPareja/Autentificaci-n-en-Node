81f609e4dc35fa72ae4dff145ae106aa04b8e829
Brian pareja <brian.pareja@tecsup.edu.pe>
2017-10-18 08:32:26 -0500

    aseguramiento local

--- app/models/user.js
+++ app/models/user.js
@@ -7,6 +7,7 @@ var bcrypt   = require('bcrypt-nodejs');
 var userSchema = mongoose.Schema({
 
     local            : {
+        userID       : String,
         email        : String,
         password     : String,
     },
--- config/passport.js
+++ config/passport.js
@@ -1,121 +1,144 @@
-// config/passport.js
 
-// load all the things we need
-var LocalStrategy   = require('passport-local').Strategy;
+var LocalStrategy    = require('passport-local').Strategy;
+var User       = require('../app/models/user');
+var configAuth = require('./auth'); // use this one for testing
 
-// load up the user model
-var User            = require('../app/models/user');
-
-// expose this function to our app using module.exports
 module.exports = function(passport) {
-
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
-
-    // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
+    passport.serializeUser(function (user, done) {
         done(null, user.id);
     });
 
-    // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
+       passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
             done(err, user);
         });
     });
 
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
-    passport.use('local-signup', new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField : 'email',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+    passport.use('local-login', new LocalStrategy({
+            // by default, local strategy uses username and password, we will override with userID
+            usernameField: 'userID',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
         },
-        function(req, email, password, done) {
+        function (req, userID, password, done) {
+            if (userID)
+                userID = userID.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
 
             // asynchronous
-            // User.findOne wont fire unless data is sent back
-            process.nextTick(function() {
-
-                // find a user whose email is the same as the forms email
-                // we are checking to see if the user trying to login already exists
-                User.findOne({ 'local.email' :  email }, function(err, user) {
+            process.nextTick(function () {
+                User.findOne({'local.userID': userID}, function (err, user) {
                     // if there are any errors, return the error
                     if (err)
                         return done(err);
 
-                    // check to see if theres already a user with that email
-                    if (user) {
-                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-                    } else {
+                    // if no user is found, return the message
+                    if (!user)
+                        return done(null, false, req.flash('loginMessage', 'No user found.'));
 
-                        // if there is no user with that email
-                        // create the user
-                        var newUser            = new User();
+                    if (!user.validPassword(password))
+                        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
 
-                        // set the user's local credentials
-                        newUser.local.email    = email;
-                        newUser.local.password = newUser.generateHash(password);
-
-                        // save the user
-                        newUser.save(function(err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-
+                    // all is well, return user
+                    else
+                        return done(null, user);
                 });
-
             });
 
         }));
 
-
-
-    // =========================================================================
-    // LOCAL LOGIN =============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
-    passport.use('local-login', new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField : 'email',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+    passport.use('local-signup', new LocalStrategy({
+            // by default, local strategy uses username and password, we will override with userID
+            usernameField: 'userID',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
         },
-        function(req, email, password, done) { // callback with email and password from our form
+        function (req, userID, password, done) {
 
-            // find a user whose email is the same as the forms email
-            // we are checking to see if the user trying to login already exists
-            User.findOne({ 'local.email' :  email }, function(err, user) {
-                // if there are any errors, return the error before anything else
-                if (err)
-                    return done(err);
+            process.nextTick(function () {
+                // if the user is not already logged in:
+                if (!req.user) {
+                    User.findOne({'local.userID': userID}, function (err, user) {
+                        // if there are any errors, return the error
+                        if (err)
+                            return done(err);
 
-                // if no user is found, return the message
-                if (!user)
-                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                        if (user) {
+                            return done(null, false, req.flash('signupMessage', 'That userID is already taken.'));
+                        } else {
 
-                // if the user is found but the password is wrong
-                if (!user.validPassword(password))
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                        if(password.length < 8 || password.length >120)
+                            return done(null, false, req.flash('signupMessage', 'Password is to short, Mínimo 8.'));
+                        var countMAY = false;
+                        var countMIN = false;
+                        var countCHA = false;
+                        var countNUM = false;
+                        for(i=0; i<password.length; i++){
+                            console.log(password.charCodeAt(i));
+                            if(password.charCodeAt(i) >= 33 && password.charCodeAt(i) <= 47){
+                                countCHA = true;
+                                continue;
+                            }
+                            if(password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57){
+                                countNUM = true;
+                                continue;
+                            }
+                            if(password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90){
+                                countMAY = true;
+                                continue;
+                            }
+                            if(password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122){
+                                countMIN = true;
+                                continue;
+                            }
 
-                // all is well, return successful user
-                return done(null, user);
+                        }
+                            if(countMIN,countNUM,countMAY == true){
+                                var newUser = new User();
+                                newUser.local.userID = userID;
+                                newUser.local.password = newUser.generateHash(password);
+
+                                newUser.save(function (err) {
+                                    if (err)
+                                        return done(err);
+
+                                    return done(null, newUser);
+                                });
+                            }
+                            else{
+                                console.log(countMIN,countNUM,countMAY);
+                                return done(null, false, req.flash('signupMessage', 'Put a better password, using letters, numbers and capital.'));
+                            }
+
+                        }
+
+                    });
+
+                } else if (!req.user.local.userID) {
+
+                    User.findOne({'local.userID': userID}, function (err, user) {
+                        if (err)
+                            return done(err);
+
+                        if (user) {
+                            return done(null, false, req.flash('loginMessage', 'That userID is already taken.'));
+
+                        } else {
+                            var user = req.user;
+                            user.local.userID = userID;
+                            user.local.password = user.generateHash(password);
+                            user.save(function (err) {
+                                if (err)
+                                    return done(err);
+
+                                return done(null, user);
+                            });
+                        }
+                    });
+                } else {
+                    return done(null, req.user);
+                }
+
             });
 
         }));
-
-
-};
-
+}
